4c1e92fb8fc890eac54a815136e4d8f1e63a3d0c
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2021-12-23 20:54:02 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import Control.control as control
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ perception/lane_following_model.py
@@ -0,0 +1,6 @@
+from cv2 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from . import utils
+from . import camera
+
