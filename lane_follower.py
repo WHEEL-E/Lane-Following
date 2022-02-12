802829603b7590fcbca475b59ae3f1ff7c7254dd
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-02-12 22:25:59 +0200

    add Lane follower main module

--- lane_follower.py
+++ lane_follower.py
@@ -2,11 +2,13 @@ import cv2
 import numpy as np
 import utils
 
+
 def main():
     w, h = 480, 360
     utils.create_trackbar(w, h)
-    video = 'test.mp4'
+    video = 0
     capture = cv2.VideoCapture(video)
+
     while True:
         ret, frame = capture.read()
         if ret == False:
@@ -22,20 +24,25 @@ def main():
 
         frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        points = [[w_top, h_top], [w - w_top, h_top], [w_bot, h_bot], [w - w_bot, h_bot]]
+        points = [[w_top, h_top], [w - w_top, h_top],
+                  [w_bot, h_bot], [w - w_bot, h_bot]]
 
         frame_warp = utils.get_warp(frame_hsv, points, w, h)
         warp_points = utils.warp_helper(frame_hsv, points)
 
         mask = cv2.inRange(frame_warp, lower, upper)
         result = cv2.bitwise_and(frame_warp, frame_warp, mask=mask)
-        
+
+        base_point, hist_img = utils.get_curve(mask, 0.5, 5, True)
+        curve_value = base_point - w//2
+
         mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        stack = np.hstack([frame_hsv, result, frame_warp])
+        stack = np.hstack([frame_hsv, mask, hist_img])
+
         cv2.imshow('stack', stack)
 
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
-if __name__ == '__main__':
-    main()
+    capture.release()
+    cv2.destroyAllWindows()
--- perception/lane_following_model.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from tensorflow import keras
-import tensorboard
-import numpy as np
-
-
