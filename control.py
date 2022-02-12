dff51cbabc71f33f58ad604e4f21a8701eb447fd
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-02-12 22:32:52 +0200

    Intialize control file

---
+++ control.py
@@ -0,0 +1,14 @@
+def forward(speed):
+    print('w')
+
+def left(speed):
+    print('a')
+
+def backward(speed):
+    print('s')
+
+def right(speed):
+    print('d')
+
+def stop(speed):
+    print('o')
