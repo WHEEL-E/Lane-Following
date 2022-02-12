3addb0ac6fc8414f66736a5688fde3a8dac01273
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-02-12 22:29:04 +0200

    Add driving modes

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import control.control as control
-
-def main():
-    pass
-
+import keyboard_control
+import bluetooth_control
+import lane_follower
 
 
+def select_mode(mode):
+    """
+    Select the mode of operation from mobile phone
+    0: Keyboard control
+    1: Bluetooth control
+    2: Lane following
+    3: Autonomous driving
+    """
+    if mode == 0:
+        keyboard_control.main()
+    elif mode == 1:
+        bluetooth_control.main()
+    elif mode == 2:
+        lane_follower.main()
+    elif mode == 3:
+        pass
 
 
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
+select_mode(2)
