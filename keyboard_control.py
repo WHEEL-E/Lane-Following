c8a36f111ce4deb1d6074a40a3ba8e0309684bfc
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-02-23 10:51:04 +0200

    Add delay and stop_flag to avoids hw overhead

--- keyboard_control.py
+++ keyboard_control.py
@@ -1,32 +1,42 @@
+import time
+
 import pygame
 
 import control
 
 
 def getKey(keyName):
-    ans = False
+    ret = False
     for event in pygame.event.get():
         pass
     keyInput = pygame.key.get_pressed()
     myKey = getattr(pygame, "K_{}".format(keyName))
     if keyInput[myKey]:
-        ans = True
+        ret = True
     pygame.display.update()
-    return ans
+    return ret
 
 
-def main():
+def main(delay):
     pygame.init()
     win = pygame.display.set_mode((100, 100))
-
+    stop_flag = False
     while True:
         if getKey("UP") or getKey("w"):
             control.forward()
+            stop_flag = False
         elif getKey("LEFT") or getKey("a"):
             control.left()
+            stop_flag = False
         elif getKey("DOWN") or getKey("s"):
             control.backward()
+            stop_flag = False
         elif getKey("RIGHT") or getKey("d"):
             control.right()
+            stop_flag = False
         else:
-            control.stop()
+            if not stop_flag:
+                control.stop()
+                stop_flag = True
+
+        time.sleep(delay)
