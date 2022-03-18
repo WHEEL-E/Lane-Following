32e530eca92619bc443c01605826d3f6cee51580
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-03-18 14:58:52 +0200

    Add timeout

--- control.py
+++ control.py
@@ -1,29 +1,34 @@
+"""
+For more information on the serial module, see: https://pythonhosted.org/pyserial/index.html
+"""
+
 import serial
 
-port = serial.Serial(
+uart = serial.Serial(
     port="/dev/ttyS0",
     baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
+    writeTimeout = 1.0
 )
 
 
 def forward(speed=0.5):
-    port.write(b'\w')
+    uart.write(b'\w')
 
 
 def left():
-    port.write(b'\a')
+    uart.write(b'\a')
 
 
 def backward(speed=0.5):
-    port.write(b'\s')
+    uart.write(b'\s')
 
 
 def right():
-    port.write(b'\d')
+    uart.write(b'\d')
 
 
 def stop():
-    port.write(b'\o')
+    uart.write(b'\o')
