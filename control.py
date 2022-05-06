e177612327b23b699eb3edfba2811c5d4bcc4923
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-05-06 09:26:00 +0200

    Split the Intializing of serial port to a function

--- control.py
+++ control.py
@@ -4,31 +4,36 @@ For more information on the serial module, see: https://pythonhosted.org/pyseria
 
 import serial
 
-uart = serial.Serial(
-    port="/dev/ttyS0",
-    baudrate=115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    writeTimeout=1.0,
-)
+def main() -> None:
+    """
+    Initialize the serial connection
+    """
+    global uart
+    uart = serial.Serial(
+        port="/dev/ttyS0",
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        writeTimeout=1.0,
+    )
 
 
 def forward(speed=0.5):
-    uart.write(b"\w")
+    uart.write(b"5w")
 
 
 def left():
-    uart.write(b"\a")
+    uart.write(b"a")
 
 
 def backward(speed=0.5):
-    uart.write(b"\s")
+    uart.write(b"5s")
 
 
 def right():
-    uart.write(b"\d")
+    uart.write(b"d")
 
 
 def stop():
-    uart.write(b"\o")
+    uart.write(b"o")
