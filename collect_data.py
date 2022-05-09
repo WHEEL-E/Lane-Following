bc65cae8920bbe569c0ccf0cc7614af4869e3619
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-05-09 09:06:21 +0200

    Handle Keyboard Interrupt exception

--- collect_data.py
+++ collect_data.py
@@ -14,7 +14,7 @@ def take_snapshot(frame, path):
     """
     Take a snapshot of the current frame and save it to the given path
     """
-    file_name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S%f") + ".jpg"
+    file_name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S%f.jpg")
     if not os.path.exists(path):
         os.makedirs(path)
     cv2.imwrite(os.path.join(path, file_name), frame)
@@ -26,44 +26,47 @@ def main(delay: float = 0.0, preview: bool = False):
     and collect data for training the model.
     """
     pygame.init()
-    win = pygame.display.set_mode((100, 100))
+    pygame.display.set_mode((100, 100))
     stop_flag = True
-    while True:
-        frame = camera.capture()
-        if preview:
-            camera.preview(frame, fps=True)
 
-        if keyboard_control.getKey("UP") or keyboard_control.getKey("w"):
-            control.forward()
-            take_snapshot(frame, "data/raw/forward")
-            stop_flag = False
-        elif keyboard_control.getKey("LEFT") or keyboard_control.getKey("a"):
-            control.left()
-            take_snapshot(frame, "data/raw/left")
-            stop_flag = False
-        elif keyboard_control.getKey("DOWN") or keyboard_control.getKey("s"):
-            control.backward()
-            stop_flag = False
-        elif keyboard_control.getKey("RIGHT") or keyboard_control.getKey("d"):
-            control.right()
-            take_snapshot(frame, "data/raw/right")
-            stop_flag = False
-        elif keyboard_control.getKey("SPACE"):
-            if not stop_flag:
-                control.stop()
-                stop_flag = True
-            take_snapshot(frame, "data/raw/stop")
-        elif keyboard_control.getKey("ESCAPE"):
-            control.stop()
-            pygame.quit()
-            return
-        else:
-            if not stop_flag:
-                control.stop()
-                stop_flag = True
+    try:
+        while True:
+            frame = camera.capture()
+            if preview:
+                camera.preview(frame, fps=True)
 
-        time.sleep(delay)
+            if keyboard_control.getKey("UP") or keyboard_control.getKey("w"):
+                control.forward()
+                take_snapshot(frame, "data/raw/forward")
+                stop_flag = False
+            elif keyboard_control.getKey("LEFT") or keyboard_control.getKey("a"):
+                control.left()
+                take_snapshot(frame, "data/raw/left")
+                stop_flag = False
+            elif keyboard_control.getKey("DOWN") or keyboard_control.getKey("s"):
+                control.backward()
+                stop_flag = False
+            elif keyboard_control.getKey("RIGHT") or keyboard_control.getKey("d"):
+                control.right()
+                take_snapshot(frame, "data/raw/right")
+                stop_flag = False
+            elif keyboard_control.getKey("SPACE"):
+                if not stop_flag:
+                    control.stop()
+                    stop_flag = True
+                take_snapshot(frame, "data/raw/stop")
+            else:
+                if not stop_flag:
+                    control.stop()
+                    stop_flag = True
+            time.sleep(delay)
+
+    except KeyboardInterrupt:
+        control.stop()
+        camera.cap.release()
+        cv2.destroyAllWindows()
+        pygame.quit()
 
 
 if __name__ == "__main__":
-    main(preview=True)
+    main(delay=0.2, preview=True)
