095a493fde8e8da9fc9da2b428e5d17f0450a027
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-05-09 09:08:48 +0200

    - Add Keyboard Interrupt exception - Remove set_stream function call due to performance issues - Use profile for benchmarking and diagnosing

--- lane_follower.py
+++ lane_follower.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import camera
 import control
+import profiling
 import utils
 
 
@@ -29,9 +30,9 @@ def take_action(
     return stop_flag
 
 
+@profiling.profile
 def main(preview: bool = False, intialize: bool = False, flip: bool = True):
     stop_flag = False
-    camera.set_stream(camera.width, camera.height, camera.fps)
 
     if intialize:
         utils.create_trackbar(camera.width, camera.height)
@@ -49,56 +50,62 @@ def main(preview: bool = False, intialize: bool = False, flip: bool = True):
             h_bot,
         ) = utils.load_values()
 
-    while True:
-        frame = camera.capture()
-        frame = camera.resize(frame, camera.width, camera.height)
+    try:
+        while True:
+            frame = camera.capture()
+            frame = camera.resize(frame, camera.width, camera.height)
 
-        if flip:
-            frame = cv2.flip(frame, 1)
+            if flip:
+                frame = cv2.flip(frame, 1)
 
-        if intialize:
-            (
-                h_min,
-                h_max,
-                s_min,
-                s_max,
-                v_min,
-                v_max,
-                w_top,
-                w_bot,
-                h_top,
-                h_bot,
-            ) = utils.get_trackbar()
+            if intialize:
+                (
+                    h_min,
+                    h_max,
+                    s_min,
+                    s_max,
+                    v_min,
+                    v_max,
+                    w_top,
+                    w_bot,
+                    h_top,
+                    h_bot,
+                ) = utils.get_trackbar()
 
-        lower = np.array([h_min, s_min, v_min])
-        upper = np.array([h_max, s_max, v_max])
-        points = [
-            [w_top, h_top],
-            [camera.width - w_top, h_top],
-            [w_bot, h_bot],
-            [camera.width - w_bot, h_bot],
-        ]
+            lower = np.array([h_min, s_min, v_min])
+            upper = np.array([h_max, s_max, v_max])
+            points = [
+                [w_top, h_top],
+                [camera.width - w_top, h_top],
+                [w_bot, h_bot],
+                [camera.width - w_bot, h_bot],
+            ]
 
-        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        frame_warp = camera.get_warp(frame_hsv, points, camera.width, camera.height)
-        mask = cv2.inRange(frame_warp, lower, upper)
-        result = cv2.bitwise_and(frame_warp, frame_warp, mask=mask)
-        camera.warp_helper(frame_hsv, points)
+            frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            frame_warp = camera.get_warp(frame_hsv, points, camera.width, camera.height)
+            mask = cv2.inRange(frame_warp, lower, upper)
+            result = cv2.bitwise_and(frame_warp, frame_warp, mask=mask)
+            camera.warp_helper(frame_hsv, points)
 
-        base_point, hist_img = utils.get_histogram(mask, 0.5, 8)
-        curve_value = base_point - camera.width // 2
+            base_point, hist_img = utils.get_histogram(mask, 0.5, 8)
+            curve_value = base_point - camera.width // 2
 
-        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+            mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
-        result = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
+            result = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
 
-        if preview:
-            stack = np.hstack([mask, result, hist_img])
-            camera.preview(stack, fps=True)
+            if preview:
+                stack = np.hstack([mask, result, hist_img])
+                camera.preview(stack)
 
-        no_lane = utils.no_lane(mask)
+            no_lane = utils.no_lane(mask)
 
-        stop_flag = take_action(no_lane, curve_value, stop_flag, min_curve=25.0)
+            stop_flag = take_action(no_lane, curve_value, stop_flag, min_curve=25.0)
+
+    except KeyboardInterrupt:
+        control.stop()
+        camera.cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
